c2b6c776beab790f6d9da71906c2c8ada84fecc6
DESKTOP-1NJKFD7\linn star <kkhakkha8@gmail.com>
2021-12-10 21:48:32 +0600

    ep 17 yield and blade engine

--- resources/views/blogs.blade.php
+++ resources/views/blogs.blade.php
@@ -1,21 +1,17 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Blogs</title>
-    <link rel="stylesheet" href="/css/app.css">
-</head>
-<body>
+
+@extends('layout')
+
+@section('content')
     <h1>My Blogs</h1>
-<?php
-    foreach($blogs as $blog): ?>
-        <h1><a href="<?= "blog/".$blog->slug ; ?>" ><?= $blog->title ;?></a></h1>
-        <p>Published at - <?= $blog->date ?></p>
-        <p><?= $blog->intro; ?></p>
+    
+        @foreach($blogs as $blog)
         
-   <?php endforeach ;
-?>
-</body>
-</html>
+            <div class="{{$loop->even ? 'bg-yellow' : ''}}">
+                <h1><a href="<?= "blog/".$blog->slug ; ?>" ><?= $blog->title ;?></a></h1>
+                <p>Published at - <?= $blog->date ?></p>
+                <p><?= $blog->intro; ?></p>
+            
+            </div>
+        @endforeach
+
+@endsection
