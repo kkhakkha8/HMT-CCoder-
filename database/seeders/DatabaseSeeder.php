54eb901e4f146ffb2bec692fd9467c6fcbbfb448
DESKTOP-1NJKFD7\linn star <kkhakkha8@gmail.com>
2021-12-25 15:10:22 +0600

    ep 31

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Blog;
+use App\Models\User;
+use App\Models\Category;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +16,33 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-    }
+        User::truncate();
+        Category::truncate();
+        Blog::truncate();
+        User::factory(10)->create();
+
+        $frontend = Category::create([
+            'name'=>"Frontend",
+            'slug'=>"front-end"
+        ]);
+        $backend = Category::create([
+            'name'=>"Backend",
+            'slug'=>"back-end"
+        ]);
+
+        Blog::create([
+            'title'=>'Front end Post',
+            'category_id'=>$frontend->id,
+            'slug'=>'front-end',
+            'intro'=>'this is intro',
+            'body'=>'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias quisquam laboriosam itaque quam vero consequatur, repellat accusantium repellendus esse aut assumenda? Tempore earum molestiae assumenda optio accusantium aspernatur vitae aliquam.'
+        ]);
+        Blog::create([
+            'title'=>'Back end Post',
+            'category_id'=>$backend->id,
+            'slug'=>'back-end',
+            'intro'=>'this is intro',
+            'body'=>'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias quisquam laboriosam itaque quam vero consequatur, repellat accusantium repellendus esse aut assumenda? Tempore earum molestiae assumenda optio accusantium aspernatur vitae aliquam.'
+        ]);
+}
 }
