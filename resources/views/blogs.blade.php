0046d8926514005d46dffbee443f241e7f017f9b
DESKTOP-1NJKFD7\linn star <kkhakkha8@gmail.com>
2021-12-31 20:26:17 +0600

    ep 36 update design

--- resources/views/blogs.blade.php
+++ resources/views/blogs.blade.php
@@ -1,3 +1,4 @@
+<!-- 
 <x-navbar></x-navbar>
 <x-layout>
     <x-slot name='title'>
@@ -9,17 +10,30 @@
         
             <div>
                 
-                <h1><a href="<?= "blog/".$blog->slug ; ?>" ><?= $blog->title ;?></a></h1>
+                <h1><a href=" {{"blog/".$blog->slug}}" >{{$blog->title}}</a></h1>
                 <p>
                     <h4>Author - <a href="/authors/{{$blog->author->username}}">{{$blog->author->name}}</h4></a>
                 </p>
                 <p>
-                    <a href="<?= "/categories/".$blog->category->slug;?>"><?= $blog->category->name ?></a>
+                    <a href="{{"/categories/".$blog->category->slug}}"> {{$blog->category->name}}</a>
                 </p>
-                <p>Published at - <?= $blog->created_at->diffForHumans() ?></p>
-                <p><?= $blog->intro; ?></p>
+                <p>Published at - {{$blog->created_at->diffForHumans()}}</p>
+                <p>{{$blog->intro}}</p>
             </div>
         @endforeach
     </x-slot>
+</x-layout>    -->
+
+
+  <x-layout>
+    <x-navbar></x-navbar>
+    <x-hero />
+    <x-blog-section />
+    <x-subscribe />
 </x-layout>
+
+
+ 
+   
+
 
--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,3 +1,10 @@
-<a href="/welcome">Welcome</a>
-<a href="/">Home</a>
-<a href="/">Back</a>
+<nav class="navbar navbar-dark bg-dark">
+      <div class="container">
+        <a class="navbar-brand" href="./index.html">Creative Coder</a>
+        <div class="d-flex">
+          <a href="#home" class="nav-link">Home</a>
+          <a href="#blogs" class="nav-link">Blogs</a>
+          <a href="#subscribe" class="nav-link">Subscribe</a>
+        </div>
+      </div>
+</nav>
