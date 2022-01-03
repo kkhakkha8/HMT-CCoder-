0dba2b977eb7192cdd1da999993c3550ce475a58
DESKTOP-1NJKFD7\linn star <kkhakkha8@gmail.com>
2022-01-03 11:41:01 +0600

    ep_37_make_blog_dynamic

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -1,13 +1,12 @@
-<x-navbar></x-navbar>
+
+
 <x-layout>
-    <x-slot name='title'>
-        <title>{{$blog->title}}</title>
-    </x-slot>
-    <x-slot name='content'>
-    <h1>{{$blog->title}}</h1>
-    <p>{{$blog->body}}</p>
-    <a href="/">go back</a>
-    </x-slot>
+    <x-navbar />
+
+        <h1>{{$blog->title}}</h1>
+        <p>{{$blog->body}}</p>
+        <a href="/">go back</a>
+
 </x-layout>
 
 
--- resources/views/blogs.blade.php
+++ resources/views/blogs.blade.php
@@ -1,39 +1,13 @@
-<!-- 
-<x-navbar></x-navbar>
-<x-layout>
-    <x-slot name='title'>
-        <title>All Blogs</title>
-    </x-slot>
-    <x-slot name='content'>
-        <h1>My Blogs</h1>
-        @foreach($blogs as $blog)
-        
-            <div>
-                
-                <h1><a href=" {{"blog/".$blog->slug}}" >{{$blog->title}}</a></h1>
-                <p>
-                    <h4>Author - <a href="/authors/{{$blog->author->username}}">{{$blog->author->name}}</h4></a>
-                </p>
-                <p>
-                    <a href="{{"/categories/".$blog->category->slug}}"> {{$blog->category->name}}</a>
-                </p>
-                <p>Published at - {{$blog->created_at->diffForHumans()}}</p>
-                <p>{{$blog->intro}}</p>
-            </div>
-        @endforeach
-    </x-slot>
-</x-layout>    -->
-
 
   <x-layout>
     <x-navbar></x-navbar>
     <x-hero />
-    <x-blog-section />
+    <x-blog-section :blogs="$blogs" />
     <x-subscribe />
 </x-layout>
 
 
- 
-   
+
+
 
 
