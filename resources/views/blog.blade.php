9845374aab100654e41e6e0810690bc69cb5ec50
DESKTOP-1NJKFD7\linn star <kkhakkha8@gmail.com>
2022-01-03 21:55:32 +0600

    ep_39_drop_down

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -3,24 +3,25 @@
     <x-navbar />
         <!-- single blog section -->
         <div class="container">
-          <div class="row">
-            <div class="col-md-6 mx-auto text-center">
-              <img
-                src="https://creativecoder.s3.ap-southeast-1.amazonaws.com/blogs/GOLwpsybfhxH0DW8O6tRvpm4jCR6MZvDtGOFgjq0.jpg"
-                class="card-img-top"
-                alt="..."
-              />
-              <h3 class="my-3">{{$blog->title}}</h3>
-              <div>Author - {{$blog->author->name}}</div>
-              <div class="badge bg-primary">{{$blog->category->name}}</div>
-              <div class="text-secondary">{{$blog->created_at->diffForHumans()}}</div>
+            <div class="row">
+              <div class="col-md-6 mx-auto text-center">
+                <img
+                  src="https://creativecoder.s3.ap-southeast-1.amazonaws.com/blogs/GOLwpsybfhxH0DW8O6tRvpm4jCR6MZvDtGOFgjq0.jpg"
+                  class="card-img-top"
+                  alt="..."
+                />
+                <h3 class="my-3">{{$blog->title}}</h3>
+                <div>Author - <a href="/authors/{{$blog->author->username}}">{{$blog->author->name}}</a></div>
+                <div class="badge bg-primary">{{$blog->category->name}}</div>
+                <div class="text-secondary">{{$blog->created_at->diffForHumans()}}</div>
 
-              <p class="lh-md mt-3">
-                {{$blog->body}}
-              </p>
+                <p class="lh-md mt-3">
+                  {{$blog->body}}
+                </p>
+              </div>
             </div>
           </div>
-        </div>
+
 
         <!-- subscribe new blogs -->
         <x-subscribe />
